579e816efb3274fd1b7ec6d0f7859c76504da316
Krzysztof Klimczak <l2eu@wp.pl>
2020-07-27 21:36:57 +0200

    Injecting The Book model

--- app.js
+++ app.js
@@ -6,35 +6,12 @@ const db=mongoose.connect('mongodb://localhost/bookAPI');
 
 const port=process.env.PORT||3000;
 const Book=require('./models/bookModel')
-const bookRouter=express.Router();
+const bookRouter=require('./routes/bookRouter')(Book);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-bookRouter.route('/books')
-.post((req,res)=>{const book=new Book(req.body);
- book.save();
-    return res.status(201).json(book);
-})
 
-.get((req,res)=>{
-    const query={};
-    if (req.query.genre){
-        query.genre=req.query.genre
-    }
-    Book.find(query,(err,books)=>{
-if (err){return res.send(err);}
-res.json(books);
-    });})
-
-
-
-    bookRouter.route('/books/:bookId').get((req,res)=>{
-        
-        Book.findById(req.params.bookId,(err,book)=>{
-    if (err){return res.send(err);}
-    res.json(book);
-        });})
 
 
         app.use('/api',bookRouter);
