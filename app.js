01be0bdff620e1f9e144efb0690b0302b29f002b
Krzysztof Klimczak <l2eu@wp.pl>
2020-07-27 12:05:56 +0200

    Wiring up to MongoDB

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express=require('express');
 const app=express();
+const mongoose=require('mongoose');
+const db=mongoose.connect('mongodb://localhost/bookAPI')
 
 const port=process.env.PORT||3000;
-
+const Book=require('./models/bookModel')
 const bookRouter=express.Router();
 
-bookRouter.route('/books').get((req,res)=>{const response={hello:'This is my api'};
-                                                res.json(response)})
+bookRouter.route('/books').get((req,res)=>{
+    Book.find((err,books)=>{
+if (err){return res.send(err);}
+res.json(books);
+    });})
         app.use('/api',bookRouter);
 app.get('/', (req,res)=>{
 res.send('Welcome to my API');
