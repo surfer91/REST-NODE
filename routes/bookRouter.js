cd9fa58b6a384ad67ef55be4a206aaf4abc05494
Krzysztof Klimczak <l2eu@wp.pl>
2020-07-28 11:26:42 +0200

    Patch

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -21,28 +21,46 @@ function routes(Book) {
         })
 
 
-bookRouter.use('/books/:bookId',(req,res,next)=>{
-    Book.findById(req.params.bookId, (err, book) => {
-        if (err) { return res.send(err); }
-        if (book){
-            req.book=book;
-            return next();
-        }
-        return res.sendStatus(404);
-    });
-})
-    bookRouter.route('/books/:bookId').get((req, res) => res.json(req.book))
-    .put((req,res)=>{ const{book}=req;
-book.title=req.body.title;
-book.author=req.body.author;
-book.genre=req.body.genre;
-book.read=req.body.read;
-book.save();
-
-           return res.json(book);
+    bookRouter.use('/books/:bookId', (req, res, next) => {
+        Book.findById(req.params.bookId, (err, book) => {
+            if (err) { return res.send(err); }
+            if (book) {
+                req.book = book;
+                return next();
+            }
+            return res.sendStatus(404);
         });
+    })
+    bookRouter.route('/books/:bookId').get((req, res) => res.json(req.book))
+        .put((req, res) => {
+            const { book } = req;
+            book.title = req.body.title;
+            book.author = req.body.author;
+            book.genre = req.body.genre;
+            book.read = req.body.read;
+            req.book.save((err)=>{
+                if (err){
+                    return res.send(err);
+                }
+                return res.json(book);
+            })
+        })
 
-  
+.patch((req,res)=>{
+    const{book}=req;
+    Object.entries(req.body).forEach(item=>{
+        const key=item[0];
+        const value=item[1];
+        book[key]=value;})
+
+        req.book.save((err)=>{
+            if (err){
+                return res.send(err);
+            }
+            return res.json(book);
+        })
+
+})
     return bookRouter;
 }
 
